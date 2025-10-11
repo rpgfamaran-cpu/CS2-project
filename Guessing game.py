7becad6e90b15bf31002df7f816033be22dfae84
half-wes <cfflaviano@mrc.pshs.edu.ph>
2025-10-11 12:20:46 +0800

    Update Guessing game.py

--- Guessing game.py
+++ Guessing game.py
@@ -4,10 +4,10 @@
 
 #price guessing game!
 #credentials stuff like login and signups idk if i should add pass and stuff
-Name1 = str(input("Input your name!"))
-print("Wow! you made a great name", Name1)
+Name1 = str(input("Input your name! "))
+print("Wow! you made a great name,", Name1)
 #game starter
-Main_Roundstarter = int(input("Would you like to start? press 1 if yes, 2 if no."))
+Main_Roundstarter = int(input("Would you like to start? press 1 if yes, 2 if no. "))
 if Main_Roundstarter == 1:
     print("Ok! lets start. Here's round 1.")
 elif Main_Roundstarter ==2:
@@ -18,4 +18,5 @@ else:
     quit()
 #idk what to name
 Mainlist = ["Headless", "Rainbow Shaggy", "Golden Crown", "Monopoly hat"]
+
 print("Please choose from this list! USE ONLY 1-5!!!", Mainlist)
