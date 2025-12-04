e6b23004bd0a402411a27455adebf845cdf10c1c
rpgfamaran-cpu <rpgfamaran@mrc.pshs.edu.ph>
2025-12-04 15:32:46 +0800

    Implement class and student management in laptop game
    Refactored the laptop assignment game to manage classes and students.

--- Wrong laptop problem.py
+++ Wrong laptop problem.py
@@ -1,23 +1,60 @@
-#Testing different stuff for reviewing.
-#Goals:
-#Input/Output included, arith, logical, relational.
-
-#price guessing game!
-#credentials stuff like login and signups idk if i should add pass and stuff
-Name1 = str(input("Input your name! "))
-print("Wow! you made a great name,", Name1)
-#game starter
-Main_Roundstarter = int(input("Would you like to start? press 1 if yes, 2 if no. "))
-if Main_Roundstarter == 1:
-    print("Ok! lets start. Here's round 1.")
-elif Main_Roundstarter ==2:
-    print("Goodbye!")
-    quit()
-else:
-    print("Invalid choice!")
-    quit()
-#idk what to name
-Mainlist = ["Headless", "Rainbow Shaggy", "Golden Crown", "Monopoly hat"]
-
-print("Please choose from this list! USE ONLY 1-5!!!", Mainlist)
-
+#Idk Quarter Project 2 I guesse. Made by Paulo, Cowey, Aidan.
+print("SUPER SIMPLE LAPTOP ASSIGNER")
+classes = {}
+while True:
+    print("\nCLASSES:")
+    num = 1
+    names = []
+    for c in classes:
+        print(num, ")", c)
+        names.append(c)
+        num += 1
+    print("\n+ = add class")
+    choice = input("Choose class number or +: ")
+    if choice == "+":
+        cname = input("Class name: ")
+        classes[cname] = []
+        print("Added class!")
+    elif choice.isdigit():
+        choice = int(choice)
+        if 1 <= choice <= len(names):
+            cname = names[choice-1]
+            while True:
+                print("\nCLASS:", cname)
+                print("STUDENTS:")
+                if len(classes[cname]) == 0:
+                    print("None")
+                else:
+                    for i in range(len(classes[cname])):
+                        print(i+1, ")", classes[cname][i][0], "-", classes[cname][i][1])
+                print("\n+ = add student")
+                print("- = remove student")
+                print("* = back")
+                pick = input("Pick: ")
+                if pick == "+":
+                    n = input("Student name: ")
+                    l = input("Laptop: ")
+                    classes[cname].append([n, l])
+                    print("Added!")
+                elif pick == "-":
+                    if len(classes[cname]) == 0:
+                        print("No one to remove")
+                    else:
+                        for i in range(len(classes[cname])):
+                            print(i+1, ")", classes[cname][i][0])
+                        r = input("Number: ")
+                        if r.isdigit():
+                            r = int(r)
+                            if 1 <= r <= len(classes[cname]):
+                                classes[cname].pop(r-1)
+                                print("Removed!")
+                            else:
+                                print("Bad number")
+                elif pick == "*":
+                    break
+                else:
+                    print("Nope.")
+        else:
+            print("Bad class.")
+    else:
+        print("Nope.")
